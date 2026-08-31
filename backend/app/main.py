19a65a1f77808e48a573452b69b511bb87516ee8
rohomshruti <147561814+rohomshruti@users.noreply.github.com>
2026-08-31 15:44:22 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,9 +19,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:8080",
-        "http://localhost:5173",
-    ],
+    "http://localhost:8080",
+    "http://localhost:5173",
+    "https://resumeiq-3r15ri4n5-rohomshruti-5504s-projects.vercel.app",
+    ]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
