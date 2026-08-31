d52cebbe6a2e68b115075b24b23f51aa6909c81a
rohomshruti <147561814+rohomshruti@users.noreply.github.com>
2026-08-31 16:11:05 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8080",
         "http://localhost:5173",
-        "https://resumeiq-3r15ri4n5-rohomshruti-5504s-projects.vercel.app",
+        "https://resumeiq-mu-six.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
